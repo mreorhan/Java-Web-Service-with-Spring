653233d02f7c06a6db9bf00ab478fc3a88547610
mreorhan <videokod7@gmail.com>
2019-06-01 16:39:07 +0300

    Add Email Check

--- src/main/java/com/mreorhan/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/mreorhan/ws/service/impl/UserServiceImpl.java
@@ -1,9 +1,10 @@
 package com.mreorhan.ws.service.impl;
 
-import com.mreorhan.ws.shared.dto.UserDto;
 import com.mreorhan.ws.entity.UserEntity;
-import com.mreorhan.ws.shared.UserRepository;
 import com.mreorhan.ws.service.UserService;
+import com.mreorhan.ws.shared.UserRepository;
+import com.mreorhan.ws.shared.Utils;
+import com.mreorhan.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,21 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    Utils utils;
 
     @Override
     public UserDto createUser(UserDto user) {
+
+        if (userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Email already exists.");
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user,userEntity); // userDTO ve userEntity eşleşmesi yapar.
 
+        String publicUserId = utils.generateUserId(30);
         userEntity.setEmailVerificationToken("123");
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testuserID");
+        userEntity.setUserId(publicUserId);
 
         UserEntity storedUserDetails= userRepository.save(userEntity);
 
