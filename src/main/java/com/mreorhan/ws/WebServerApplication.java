a57ac4425c70a63df34e4740e2ed7b3d8b48d034
mreorhan <videokod7@gmail.com>
2019-06-02 16:43:31 +0300

    Change Secret Token Method

--- src/main/java/com/mreorhan/ws/WebServerApplication.java
+++ src/main/java/com/mreorhan/ws/WebServerApplication.java
@@ -1,5 +1,6 @@
 package com.mreorhan.ws;
 
+import com.mreorhan.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +21,10 @@ public class WebServerApplication {
     @Bean
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
+    }
+
+    @Bean(name = "AppProperties")
+    public AppProperties getAppProperties() {
+        return new AppProperties();
     }
 }
--- src/main/java/com/mreorhan/ws/security/SecurityConstants.java
+++ src/main/java/com/mreorhan/ws/security/SecurityConstants.java
@@ -1,12 +1,19 @@
 package com.mreorhan.ws.security;
 
+import com.mreorhan.ws.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000;//10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "dghjafsdfkjsdf";
 
     public static final String USER_URL = "/users/";
     public static final String LOGIN_URL = USER_URL + "login";
+
+
+    public static String getTokenSecret() {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
