4bc909fb4ee2722bb3e7ae875bf0ce800281bba9
mreorhan <videokod7@gmail.com>
2019-06-02 14:08:59 +0300

    Add Authorization

--- src/main/java/com/mreorhan/ws/security/WebSecurity.java
+++ src/main/java/com/mreorhan/ws/security/WebSecurity.java
@@ -23,8 +23,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
-                .anyRequest().authenticated()
-                .and().addFilter(getAuthenticationFilter());
+                .anyRequest().authenticated().and()
+                .addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
@@ -35,8 +36,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     }
 
     public AuthenticationFilter getAuthenticationFilter() throws Exception {
-        final AuthenticationFilter filter = new AuthenticationFilter((authenticationManager()));
-        filter.setFilterProcessesUrl(SecurityConstants.LOGIN_URL);
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        String link = SecurityConstants.LOGIN_URL;
+        filter.setFilterProcessesUrl(link);
         return filter;
     }
 }
