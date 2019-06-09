4775b83e95b2bd5fcefaef8e93e3df63f24f8f42
mreorhan <videokod7@gmail.com>
2019-06-09 22:40:58 +0300

    Add war support

--- src/main/java/com/mreorhan/ws/WebServerApplication.java
+++ src/main/java/com/mreorhan/ws/WebServerApplication.java
@@ -3,11 +3,18 @@ package com.mreorhan.ws;
 import com.mreorhan.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class WebServerApplication {
+public class WebServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebServerApplication.class, args);
